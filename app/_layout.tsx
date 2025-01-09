4108bd4b0da586634afdbdeb9b7f5c5369d73fc5
abhishek-2k23 <kumarabhishek00090@gmail.com>
2025-01-10 00:52:16 +0530

    welcome test with themed view

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,8 @@
+import { Colors } from "@/constants/Colors";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const theme = useColorScheme() || 'light';
+  return <Stack screenOptions={{headerShown: false, statusBarBackgroundColor: Colors[theme].background}}/>;
 }
